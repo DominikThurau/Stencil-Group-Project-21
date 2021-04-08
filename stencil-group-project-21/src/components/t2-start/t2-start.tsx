b1239a9e212cf725f6d7c4c1d3152800437a01a2
TabeaWoehr <tabi.woehr@gmail.com>
2021-04-08 22:49:43 +0200

    started putting first html-test-elements in there

--- stencil-group-project-21/src/components/t2-start/t2-start.tsx
+++ stencil-group-project-21/src/components/t2-start/t2-start.tsx
@@ -6,13 +6,24 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class T2Start {
-
   render() {
     return (
       <Host>
-        <slot></slot>
+        <slot>
+          <div>
+            <img></img>
+            <div>
+              <p id="time">45:68</p>
+              <hr></hr>
+              <p id="date">40.15.88</p>
+              <p id="worldDay">internationaler Tag des Fülltextes</p>
+              <img id="weatherIcon"></img>
+              <p id="weather">80 °C</p>
+            </div>
+            <img id="scrollButton"></img>
+          </div>
+        </slot>
       </Host>
     );
   }
-
 }
