a7bcbca1b080f67f0c14bc510762ea36ce05fa11
lauraschrmm <lauraschramm@t-online.de>
2021-04-09 16:03:59 +0200

    To-Do-Liste HTML & CSS angelegt

--- stencil-group-project-21/src/components/t2-todo/t2-todo.tsx
+++ stencil-group-project-21/src/components/t2-todo/t2-todo.tsx
@@ -10,7 +10,19 @@ export class T2Todo {
   render() {
     return (
       <Host>
+        <div class="container">
+          <h1>Meine To-Do's</h1><button type="button" class="button-hinzu">+ Kategorie hinzufügen</button>
+          <div class="platzhalter">leer</div>
+          <div class="backgroundcolor-todo">
+            <div class="box-studium">Studium</div>
+            <div class="box-arbeit">Arbeit</div>
+            <div class="box-privat">Privat</div>
+            <div class="box-notizen">Notizen</div>
+            <div class="kreis">A</div>
+            <div class="pfeil-footer">ᐱ</div>
+          </div>
         <slot></slot>
+        </div>
       </Host>
     );
   }
