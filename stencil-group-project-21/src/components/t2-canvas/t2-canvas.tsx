abfb4ce0136b7f43c9b5331692355df5a7341e13
Dominik Thurau <dominik.thurau@ueberbit.de>
2021-04-12 23:04:15 +0200

    A working canvas

--- stencil-group-project-21/src/components/t2-canvas/t2-canvas.tsx
+++ stencil-group-project-21/src/components/t2-canvas/t2-canvas.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Listen } from '@stencil/core';
 
 @Component({
   tag: 't2-canvas',
@@ -9,36 +9,119 @@ import { Component, Host, h } from '@stencil/core';
 export class T2Canvas {
   canvas!: HTMLCanvasElement;
   context: CanvasRenderingContext2D;
-  isTouching!:boolean;
+  paint:boolean = false;
+  coord: {x:number , y:number};
+  pencilColors;
+  pencilIndex: number;
+  brushSize: number;
 
-
-  getMousePosition(event) {
-    let rect = this.canvas.getBoundingClientRect();
-    let x = event.clientX - rect.left;
-    let y = event.clientY - rect.top;
-    console.log("Coordinate x: " + x, "Coordinate y: " + y);
-    this.draw(x,y);
-}
   
-  draw(mouseX: number, mouseY: number) {
-    console.log("Touched");
-    this.context.beginPath();
-    this.context.arc(mouseX, mouseY, 5, 0, 2*Math.PI);
-    this.context.stroke();
+  constructor() {
+    //Fuck this
+    this.startPainting = this.startPainting.bind(this);
+    this.changeBrushSize = this.changeBrushSize.bind(this);
+    this.changeColor = this.changeColor.bind(this);
+    this.clearCanvas = this.clearCanvas.bind(this);
+
+    this.coord = {x:0 , y:0}; 
+    this.pencilColors =["green", "red", "blue"];
+    this.pencilIndex = 0;
+    this.brushSize = 2;
+  }
+  
+  changeBrushSize(){
+    console.log(this.brushSize);
+    if(this.brushSize<8){
+      this.brushSize += 2;
+    } else {
+      this.brushSize = 2;
+    }
+    this.context.lineWidth = this.brushSize;
+  }
+
+  changeColor(){
+    if(this.pencilIndex<this.pencilColors.length -1){
+      this.pencilIndex ++;
+    } else {
+      this.pencilIndex = 0;
+    }
+    this.context.strokeStyle = this.pencilColors[this.pencilIndex];
+  }
+
+  clearCanvas(){
+    this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
   }
 
   componentDidRender(){
-    console.log(this.canvas);
     this.context = this.canvas.getContext("2d");
-    let that = this;
-    this.canvas.addEventListener("mousedown", function(e)
-    {
-      
-        that.getMousePosition(e);
-    });
-    
+
+    this.resize(); // Resizes the canvas once the window loads
+    document.addEventListener('mousedown', this.startPainting);
+    document.addEventListener('mouseup', this.stopPainting);
+    document.addEventListener('mousemove', this.sketch);
+    document.addEventListener('resize', this.resize);
+
     //this.el.addEventListener("mousedown", this.draw, false);
   }
+
+  public getMousePos(event) {
+    let rect = this.canvas.getBoundingClientRect();
+    this.coord.x = (event.clientX - rect.left);
+    this.coord.y = event.clientY - rect.top;
+    console.log(this.coord);
+    //console.log("Coordinate x: " + x, "Coordinate y: " + y);
+  }
+
+  @Listen("mousedown")
+  startPainting(event) {
+    console.log(this.pencilColors[0])
+    this.paint = true;
+    this.getMousePos(event);
+  }
+
+  @Listen("mouseup")
+  stopPainting(){
+    this.paint = false;
+  }
+
+  @Listen("mousemove")
+  sketch(event){
+
+    if (!this.paint) return;
+    console.log("Move");
+    this.context.beginPath();
+      
+    
+     
+    // Sets the end of the lines drawn
+    // to a round shape.
+    this.context.lineCap = 'round';
+      
+
+        
+    // The cursor to start drawing
+    // moves to this coordinate
+    this.context.moveTo(this.coord.x, this.coord.y);
+     
+    // The position of the cursor
+    // gets updated as we move the
+    // mouse around.
+    this.getMousePos(event);
+     
+    // A line is traced from start
+    // coordinate to this coordinate
+    this.context.lineTo(this.coord.x , this.coord.y);
+      
+    // Draws the line.
+    this.context.stroke();
+  }
+
+  resize(){
+    this.context.canvas.width = 300;
+    this.context.canvas.height = 500;
+  }
+
+
 
   render() {
 
@@ -51,11 +134,11 @@ export class T2Canvas {
           <h2>Canvas</h2>
           <canvas id="canvas" width="300" height="500" class="shadow" ref={(el) => this.canvas = el as HTMLCanvasElement}></canvas>
           <div class="button-bar">
+            <div class="icon-button shadow" onClick={this.changeColor}><p>C</p></div>
+            <div class="icon-button shadow" onClick={this.changeBrushSize}><p>W</p></div>
             <div class="icon-button shadow"><p></p></div>
             <div class="icon-button shadow"><p></p></div>
-            <div class="icon-button shadow"><p></p></div>
-            <div class="icon-button shadow"><p></p></div>
-            <div class="icon-button shadow"><p></p></div>
+            <div class="icon-button shadow" onClick={this.clearCanvas}><p>X</p></div>
           </div>
         </div>
         
