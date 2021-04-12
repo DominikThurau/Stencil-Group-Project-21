057c5dd9f4a2526df560a3d4f67a939a8915e729
TabeaWoehr <tabi.woehr@gmail.com>
2021-04-12 22:41:55 +0200

    put in Date, Time and first approach on Weather

--- stencil-group-project-21/src/components/t2-start/t2-start.tsx
+++ stencil-group-project-21/src/components/t2-start/t2-start.tsx
@@ -6,16 +6,36 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class T2Start {
+  getWeather(cityID) {
+    const apiKey: string = '501ed5e2d77d2c5c368e797804806020';
+    fetch('api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + apiKey).then(response => console.log(response));
+  }
+
+  /*
+  Zeit orientiert an URL: https://gist.github.com/Konstantinos-infogeek/8f89e07139bc3dacea371e3cf2bc556c
+  */
+  Time() {
+    let date = new Date();
+    return [date.getHours(), date.getMinutes()].map(current => (current >= 10 ? current : '0' + current)).join(':');
+  }
+
+  /*
+  Datum orientiert an URL: https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
+  */
+  Date() {
+    let today = new Date().toLocaleDateString();
+    return today;
+  }
+
   render() {
     return (
       <Host>
         <slot>
           <div>
-            <img></img>
             <div>
-              <p id="time">45:68</p>
+              <p id="time">{this.Time()}</p>
               <hr></hr>
-              <p id="date">40.15.88</p>
+              <p id="date">{this.Date()}</p>
               <p id="worldDay">internationaler Tag des Fülltextes</p>
               <img id="weatherIcon"></img>
               <p id="weather">80 °C</p>
@@ -27,3 +47,14 @@ export class T2Start {
     );
   }
 }
+
+/*
+let cityID: number = 2869117;
+
+function getWeather(cityID: number): string {
+  const apiKey: string = '501ed5e2d77d2c5c368e797804806020';
+  fetch('api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + apiKey).then(response => console.log(response));
+
+  return 'toll';
+}
+*/
