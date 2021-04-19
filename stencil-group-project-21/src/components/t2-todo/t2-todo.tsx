1c7bd33e59e3c37c23f3df3905599a987c9e28c1
lauraschrmm <lauraschramm@t-online.de>
2021-04-19 14:06:06 +0200

    refresh to-do-list

--- stencil-group-project-21/src/components/t2-todo/t2-todo.tsx
+++ stencil-group-project-21/src/components/t2-todo/t2-todo.tsx
@@ -11,13 +11,24 @@ export class T2Todo {
     return (
       <Host>
         <div class="container">
-          <h1>Meine To-Do's</h1><button type="button" class="button-hinzu">+ Kategorie hinzufügen</button>
+          <h1>Meine To-Do's</h1>
           <div class="platzhalter">leer</div>
           <div class="backgroundcolor-todo">
             <div id="kategorie" class="box-studium">Studium</div>
             <div id="kategorie" class="box-arbeit">Arbeit</div>
             <div id="kategorie" class="box-privat">Privat</div>
             <div id="kategorie" class="box-notizen">Notizen</div>
+            <textarea class="hinzufuegen" ></textarea>
+            <div class="kategorien-auswahl">
+                  <input type="radio" name="studium"/>
+                  <label>Studium</label>
+                  <input type="radio" name="arbeit"/>
+                  <label>Arbeit</label>
+                  <input type="radio" name="privat"/>
+                  <label>Privat</label>
+                  <input type="radio" name="notizen"/>
+                  <label>Notizen</label>
+                </div>
             <div class="kreis">A</div>
             <div class="pfeil-footer">ᐱ</div>
           </div>
