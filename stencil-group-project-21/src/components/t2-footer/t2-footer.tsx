633834753064b017f3b9f8d85cf3890297f02be9
Tobias Huber <tobias.huber.oppenau@outlook.de>
2021-04-19 14:11:35 +0200

    footer style changes

--- stencil-group-project-21/src/components/t2-footer/t2-footer.tsx
+++ stencil-group-project-21/src/components/t2-footer/t2-footer.tsx
@@ -14,20 +14,27 @@ export class T2Footer {
           <div class="platzhalter">leer</div>
             <div class="backgroundcolor-footer">
               <div class="bg-footer">
-                <div id="content" class="content-footer">
-                
+                <div class="content-footer">
+              <div class="line"><hr></hr></div>
+                <div id="icon" class="icon-bar">
                 <div class="icon-instagram">
                       <slot name="instagram"/>
-                  </div> 
-                  <div class="icon-spotify">
-                      <slot name="spotify"/>
                   </div>
-
+                  <div class="icon-github">
+                      <slot name="github"/>
+                  </div>
+                  <div class="icon-moodle">
+                      <slot name="moodle"/>
+                  </div>
+                  <div class="icon-calendar">
+                      <slot name="calendar"/>
+                  </div>
                     <div class="icon-download">
                       <slot name="download"/>
                   </div>
                   <div class="icon-upload">
                       <slot name="upload"/>
+                  </div>
                   </div>
                   </div>
               </div>
