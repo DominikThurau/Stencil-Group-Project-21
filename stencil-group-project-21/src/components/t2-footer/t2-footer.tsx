468f30ad969f48504d2f62f3f94e7507ca70a820
Dominik Thurau <dominik.thurau@ueberbit.de>
2021-05-03 10:54:11 +0200

    minor changes

--- stencil-group-project-21/src/components/t2-footer/t2-footer.tsx
+++ stencil-group-project-21/src/components/t2-footer/t2-footer.tsx
@@ -1,73 +1,75 @@
 import { Component, Host, h } from '@stencil/core';
 
 @Component({
-tag: 't2-footer',
-styleUrls: ['t2-footer.css', '../../assets/fontawesome/css/all.css'],
-shadow: true,
+  tag: 't2-footer',
+  styleUrls: ['t2-footer.css', '../../assets/fontawesome/css/all.css'],
+  shadow: true,
 })
+
 export class T2Footer {
   constructor() {
     this.toggleContent = this.toggleContent.bind(this);
   }
 
-hiddenContent:HTMLDivElement;
+  hiddenContent: HTMLDivElement;
 
-toggleContent(){
-this.hiddenContent.classList.toggle("hiddenContent");
-}
+  toggleContent() {
+    this.hiddenContent.classList.toggle('hiddenContent');
+  }
 
-topFunction() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
-}
+  topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 
-
-
-
-render() {
-return (
-<Host>
-  <div class="container">
-    <div class="platzhalter">leer</div>
-    <div class="backgroundcolor-footer">
-      <div class="bg-footer">
-        <div class="icon-up" id="icon-up" onClick={this.toggleContent}>
-          <div class="up"></div>
-          <slot name="up" />
-        </div>
-        <div class="visibleContent" ref={el=> (this.hiddenContent = el as HTMLDivElement)}>
-          <div id="kategorie" class="box-studium">Erbe</div>
-          <div id="kategorie" class="box-arbeit">Kontaktdaten Dozenten</div>
-          <div id="kategorie" class="box-privat">StadtLandFluss Geschichten</div>
-        </div>
-        <div class="content-footer">
-          <div class="line">
-            <hr>
-            </hr>
+  render() {
+    return (
+      <Host>
+        <div class="container">
+          <div class="platzhalter">leer</div>
+          <div class="backgroundcolor-footer">
+            <div class="bg-footer">
+              <div class="icon-up" id="icon-up" onClick={this.toggleContent}>
+                <div class="up"></div>
+                <slot name="up" />
+              </div>
+              <div class="visibleContent" ref={el => (this.hiddenContent = el as HTMLDivElement)}>
+                <div id="kategorie" class="box-studium">
+                  Erbe
+                </div>
+                <div id="kategorie" class="box-arbeit">
+                  Kontaktdaten Dozenten
+                </div>
+                <div id="kategorie" class="box-privat">
+                  StadtLandFluss Geschichten
+                </div>
+              </div>
+              <div class="content-footer">
+                <div class="line">
+                  <hr></hr>
+                </div>
+                <div id="icon" class="icon-bar">
+                  <div class="icon-instagram">
+                    <slot name="instagram" />
+                  </div>
+                  <div class="icon-github">
+                    <slot name="github" />
+                  </div>
+                  <div class="icon-moodle">
+                    <slot name="moodle" />
+                  </div>
+                  <div class="icon-calendar">
+                    <slot name="calendar" />
+                  </div>
+                  <div class="icon-download">
+                    <slot name="download" />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div id="icon" class="icon-bar">
-            <div class="icon-instagram">
-              <slot name="instagram" />
-            </div>
-            <div class="icon-github">
-              <slot name="github" />
-            </div>
-            <div class="icon-moodle">
-              <slot name="moodle" />
-            </div>
-            <div class="icon-calendar">
-              <slot name="calendar" />
-            </div>
-            <div class="icon-download">
-              <slot name="download" />
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</Host>
-);
-}
-
+      </Host>
+    );
+  }
 }
